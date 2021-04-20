133b06abe19fbdeadafee7f5318ac575fe308e70
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-04-20 08:20:13 -0600

    Add firebase auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
+
+// firebase
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    FormsModule,
+    NgxAuthFirebaseUIModule.forRoot(
+      environment.firebase,
+      () => 'your_app_name_factory',
+      {
+        enableFirestoreSync: true, // enable/disable autosync users with firestore
+        toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
+        toastMessageOnAuthError: false, // whether to open/show a snackbar message on auth error - default : true
+        authGuardFallbackURL: '/login', // url for unauthenticated users - to use in combination with canActivate feature on a route
+        authGuardLoggedInURL: '/game', // url for authenticated users - to use in combination with canActivate feature on a route
+        passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
+        passwordMinLength: 8, // Password length min/max in forms independently of each componenet min/max.
+        // Same as password but for the name
+        nameMaxLength: 50,
+        nameMinLength: 2,
+        // If set, sign-in/up form is not available until email has been verified.
+        // Plus protected routes are still protected even though user is connected.
+        guardProtectedRoutesUntilEmailIsVerified: false,
+        enableEmailVerification: false, // default: true
+        useRawUserCredential: true, // If set to true outputs the UserCredential object instead of firebase.User after login and signup - Default: false
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
