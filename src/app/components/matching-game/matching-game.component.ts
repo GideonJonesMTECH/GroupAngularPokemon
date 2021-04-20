2a7678aaadbaaaf4050c2218d311488bf91b9a4a
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-04-20 10:32:10 -0600

    Only Pokemon Available, No Duplicate Random Cards, Game Table Start

--- src/app/components/matching-game/matching-game.component.ts
+++ src/app/components/matching-game/matching-game.component.ts
@@ -11,25 +11,18 @@ export class MatchingGameComponent implements OnInit {
   constructor(private api: ApiCallService) {}
 
   matchCount = 5;
-  trainerCards = true;
   matchingCards = [];
+  selectedNumbs = [];
 
   ngOnInit(): void {
-    if (this.trainerCards) {
-      this.api
-        .apiCall((this.generateRandomNumber(1, 54) as unknown) as string, true)
-        .subscribe((data) => {
-          console.warn('get API data', data);
-          let returnData = data as ApiReturn;
-          this.setup(returnData.data);
-        });
-    } else {
-      this.api.apiCall().subscribe((data) => {
+    this.api
+      .apiCall((this.generateRandomNumber(1, 54) as unknown) as string)
+      // .apiCall('54', true) TESTING, SMALLEST DATA POOL
+      .subscribe((data) => {
         console.warn('get API data', data);
         let returnData = data as ApiReturn;
         this.setup(returnData.data);
       });
-    }
   }
 
   setup(data) {
@@ -37,16 +30,66 @@ export class MatchingGameComponent implements OnInit {
     console.log(`Card Amount: ${this.matchCount * 2}`);
     let dataLength = data.length - 1;
     for (let i = 0; i < this.matchCount; i++) {
-      let randomCard = data[this.generateRandomNumber(0, dataLength)];
-      this.matchingCards.push({
-        name: randomCard.name,
-        smlImg: randomCard.images.small,
-        lrgImg: randomCard.images.large,
-      });
+      this.createNewCard(data, dataLength);
     }
     console.log(`Cards for Matching:`, this.matchingCards);
+    let doubleCards = this.matchingCards.concat(this.matchingCards);
+    this.matchingCards = this.shuffle(doubleCards);
   }
+
   generateRandomNumber(min = 0, max = 0) {
     return Math.floor(Math.random() * max + min);
+  }
+
+  createNewCard(data, dataLength) {
+    let randNumb = this.generateRandomNumber(0, dataLength);
+    for (let j = 0; j < this.selectedNumbs.length; j++) {
+      if (randNumb == this.selectedNumbs[j]) {
+        console.log('FOUND DUPLICATE CARD, RUNNING FUNCTION AGAIN');
+        this.createNewCard(data, dataLength);
+        return;
+      }
+    }
+
+    let randomCard = data[randNumb];
+
+    if (
+      randomCard.images.large ==
+        'https://images.pokemontcg.io/hsp/HGSS18_hires.png' ||
+      randomCard.supertype !== 'Pokémon'
+    ) {
+      console.log('FOUND BAD CARD, RUNNING FUNCTION AGAIN');
+      this.createNewCard(data, dataLength);
+      return;
+    }
+
+    this.selectedNumbs.push(randNumb);
+
+    this.matchingCards.push({
+      name: randomCard.name,
+      smlImg: randomCard.images.small,
+      lrgImg: randomCard.images.large,
+      supertype: randomCard.supertype,
+    });
+  }
+
+  shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 }
