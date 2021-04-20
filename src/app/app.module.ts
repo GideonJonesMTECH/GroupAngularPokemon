4f769562a6769aac9bb36ae31f227366f2438453
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-04-20 08:54:19 -0600

    nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
-import { LoginComponent } from './componets/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 
 // firebase
