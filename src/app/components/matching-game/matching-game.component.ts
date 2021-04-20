38f1cc39833024a7fa9ce5db2ef6b78eec3ed889
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-04-20 14:04:38 -0600

    Layered Images, Larger Fades in on hover

--- src/app/components/matching-game/matching-game.component.ts
+++ src/app/components/matching-game/matching-game.component.ts
@@ -10,7 +10,7 @@ import { ApiCallService } from 'src/app/services/api-call.service';
 export class MatchingGameComponent implements OnInit {
   constructor(private api: ApiCallService) {}
 
-  matchCount = 100;
+  matchCount = 5;
   matchingCards = [];
   selectedNumbs = [];
 
