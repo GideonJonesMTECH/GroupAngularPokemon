5edffbba332a8bcb6e5baf456d5a61e16de2cd74
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-04-21 08:02:38 -0600

    created and started setup for game-settings page

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoggedInGuard } from 'ngx-auth-firebaseui';
 import { AccountComponent } from '../components/account/account.component';
+import { GameSettingsComponent } from '../components/game-settings/game-settings.component';
 import { LoginComponent } from '../components/login/login.component';
 import { MatchingGameComponent } from '../components/matching-game/matching-game.component';
 
@@ -10,6 +11,7 @@ import { MatchingGameComponent } from '../components/matching-game/matching-game
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'game', component: MatchingGameComponent, canActivate: [LoggedInGuard] },
+  { path: 'game-settings', component: GameSettingsComponent, canActivate: [LoggedInGuard] },
   { path: 'account', component: AccountComponent, canActivate: [LoggedInGuard] },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
