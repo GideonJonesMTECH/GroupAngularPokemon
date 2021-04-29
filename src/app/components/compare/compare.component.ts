37f98b7103438c247f25cc45c5f9c9fe4ef95300
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-04-29 10:08:47 -0600

    fixed styling, playerswon and players lost

--- src/app/components/compare/compare.component.ts
+++ src/app/components/compare/compare.component.ts
@@ -11,8 +11,8 @@ export class CompareComponent implements OnInit {
 
   gamesWon: number;
   gamesLost: number;
-  playersWon;
-  playersLost;
+  playersWon = [];
+  playersLost = [];
 
   constructor(
     private currentUserServe: CurrentUserService,
@@ -22,12 +22,21 @@ export class CompareComponent implements OnInit {
   user;
 
   ngOnInit(): void {
-    this.user = this.authService.getUserById(this.currentUserServe.getUser());
     this.authService.user$.subscribe(doc => {
       this.gamesWon = doc.timesWon;
       this.gamesLost = doc.timesLost;
-      this.playersWon = doc.playersWon;
-      this.playersLost = doc.playersLost;
+
+      for (let i = 0; i < doc.playersWon.length; i++) {
+        if (!this.playersWon.includes(doc.playersWon[i])) {
+          this.playersWon.push(doc.playersWon[i]);
+        }
+      }
+
+      for (let i = 0; i < doc.playersLost.length; i++) {
+        if (!this.playersLost.includes(doc.playersLost[i])) {
+          this.playersLost.push(doc.playersLost[i]);
+        }
+      }
     })
   }
 
