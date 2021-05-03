4f7441837e360c3e62f57f299d09648b55eef208
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-05-03 10:21:03 -0600

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDgn0R12jWJhz4BsZ11mj-FTBL_sUOiyzE",
+    authDomain: "pokemonmatching.firebaseapp.com",
+    projectId: "pokemonmatching",
+    storageBucket: "pokemonmatching.appspot.com",
+    messagingSenderId: "944044078865",
+    appId: "1:944044078865:web:d8b2f3c5bbcad4e08afeb0"
+  }
 };
